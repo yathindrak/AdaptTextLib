b5098eaad25f5908184b285fc5e251c1b6cb437b
Yathindra Kodithuwakku <yathindra,rawya@pearson.com>
2021-02-27 16:47:47 +0530

    Fixed path issue

--- loretex/loretex.py
+++ loretex/loretex.py
@@ -99,6 +99,8 @@ class LoReTex:
                 shutil.rmtree(str(self.mdl_path))
                 os.mkdir(str(self.mdl_path))
             else:
+                os.mkdir(str(self.data_path))
+                os.mkdir(str(self.path))
                 os.mkdir(str(self.mdl_path))
             shutil.move(source, self.mdl_path)
 
