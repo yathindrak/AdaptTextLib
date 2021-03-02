b652065cb1f2de680b1a6e63f27c0676b77a2da2
Yathindra Kodithuwakku <yathindra,rawya@pearson.com>
2021-03-02 21:18:26 +0530

    Update the Callbacks

--- loretex/trainer/classifier_trainer.py
+++ loretex/trainer/classifier_trainer.py
@@ -72,17 +72,17 @@ class ClassifierTrainer(Trainer):
         tuner = HyperParameterTuner(learn)
         lr = tuner.find_optimized_lr()
 
-        learn.fit_one_cycle(11, lr, callbacks=[SaveModelCallback(learn, every='improvement', monitor='accuracy'), ReduceLROnPlateauCallback(learn, factor=0.8)])
+        learn.fit_one_cycle(11, lr, callbacks=[SaveModelCallback(learn), ReduceLROnPlateauCallback(learn, factor=0.8)])
 
         if grad_unfreeze:
             learn.freeze_to(-2)
-            learn.fit_one_cycle(11, lr, callbacks=[SaveModelCallback(learn, every='improvement', monitor='accuracy'), ReduceLROnPlateauCallback(learn, factor=0.8)])
+            learn.fit_one_cycle(8, lr, callbacks=[SaveModelCallback(learn), ReduceLROnPlateauCallback(learn, factor=0.8)])
 
             learn.freeze_to(-3)
-            learn.fit_one_cycle(10, lr, callbacks=[SaveModelCallback(learn, every='improvement', monitor='accuracy'), ReduceLROnPlateauCallback(learn, factor=0.8)])
+            learn.fit_one_cycle(8, lr, callbacks=[SaveModelCallback(learn), ReduceLROnPlateauCallback(learn, factor=0.8)])
 
         learn.unfreeze()
-        learn.fit_one_cycle(10, lr, callbacks=[SaveModelCallback(learn, every='improvement', monitor='accuracy'), ReduceLROnPlateauCallback(learn, factor=0.8)])
+        learn.fit_one_cycle(20, lr, callbacks=[SaveModelCallback(learn, every='improvement', monitor='accuracy'), ReduceLROnPlateauCallback(learn, factor=0.8)])
 
         if self.is_backward:
             learn.save(f'{self.lang}_clas_bwd')
