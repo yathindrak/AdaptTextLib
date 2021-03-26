719243d2f2acfa80f20801aa54ecdb6a14c6ada3
Yathindra Kodithuwakku <yathindra,rawya@pearson.com>
2021-03-26 16:06:46 +0530

    Add pretrain sample code

---
+++ examples/pretrain.py
@@ -0,0 +1,10 @@
+from adapttext.adapt_text import AdaptText
+
+if __name__ == '__main__':
+	lang = 'si'
+	app_root = "/storage"
+	bs = 128
+	splitting_ratio = 0.1
+	adapttext = AdaptText(lang, app_root, bs, splitting_ratio)
+	adapttext.build_base_lm()
+	adapttext.store_lm("full_si_dedup_30k.zip")
